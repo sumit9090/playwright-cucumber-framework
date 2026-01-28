449b2a3e10667a665a30715c708a93b246cc41a0
sumit <sumit.kalra121@gmail.com>
2026-01-28 20:16:09 +0530

    added time stamp in cucumber.js and in timestamp.js

--- cucumber.js
+++ cucumber.js
@@ -1,20 +1,31 @@
+const { timestamp } = require('./utils/timestamp');
 module.exports = {
   default: {
     require: [
       'features/support/**/*.js',
       'features/step_definitions/**/*.js'
     ],
-    //tags: '@smoke',
+    tags: '@smoke',
     timeout: 60000,
     publishQuiet: true,
+    // format: [
+    //   'progress',                      // console output
+    //   'html:reports/cucumber-report.html', // optional HTML report
+    //   'allure-cucumberjs/reporter' // Allure reporter
+    // ],
+    // formatOptions: {
+    //   allureResults: 'allure-results' // folder for raw Allure results
+    // }
+
     format: [
-      'progress',                      // console output
-      'html:reports/cucumber-report.html', // optional HTML report
-      'allure-cucumberjs/reporter' // Allure reporter
-    ],
-    formatOptions: {
-      allureResults: 'allure-results' // folder for raw Allure results
-    }
+  'progress',
+  `html:reports/cucumber-report-${timestamp}.html`,
+  'allure-cucumberjs/reporter'
+],
+formatOptions: {
+  allureResults: `allure-results/${timestamp}`
+}
+
   }
 };
 
